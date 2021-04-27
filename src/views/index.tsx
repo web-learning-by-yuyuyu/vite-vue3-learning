34a3eefbb0bc06dcb73a436fa90d8918b26d5525
我不爱吃鱼鱼鱼鱼鱼 <51955207+zy94xhn@users.noreply.github.com>
2021-04-27 22:12:48 +0800

    router重写遇到难题了

---
+++ src/views/index.tsx
@@ -0,0 +1,14 @@
+import { defineComponent } from "vue";
+
+const index =  defineComponent({
+    setup () {
+        const forKey = () =>{
+            const list = ref([])
+        }
+        return () =>
+            <div>
+                <a href=""></a>
+            </div>;
+    }
+})
+export default index
