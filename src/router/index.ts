47b00e963534e893b2aecd5cd2b06e87a3e18acd
我不爱吃鱼鱼鱼鱼鱼 <51955207+zy94xhn@users.noreply.github.com>
2021-04-29 20:17:24 +0800

    GITPAGES

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import type { App } from 'vue';
 //@ts-ignore
-import {createRouter, createWebHistory,RouteRecordRaw } from "vue-router";
+import {createRouter, createWebHashHistory,RouteRecordRaw } from "vue-router";
 import {baseRoutes} from "./baseRoute/index";
 export const router = createRouter({
-  history:createWebHistory(),
+  history:createWebHashHistory(),
   routes:(baseRoutes as unknown) as RouteRecordRaw[]
 })
 export default function setupRouter (app:App<Element>) {
