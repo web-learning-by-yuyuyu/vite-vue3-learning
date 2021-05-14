98fbcc421cba0a29785e49ce3d2a283feaa0d328
wc_zhaoyang <1129728041@qq.com>
2021-05-14 11:00:47 +0800

    add: type 补充

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,32 @@
+import Vue, { VNode } from "vue"
+
 declare module "*.md" {
   import { ComponentOptions } from "vue";
   const Component: ComponentOptions;
   export default Component;
+}
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '*.scss' {
+  const scss: Record<string, string>
+  export default scss;
+}
+
+declare module '*.tsx' {
+  import Vue from 'compatible-vue'
+  export default Vue
+}
+
+declare global {
+  namespace JSX {
+    interface Element extends VNode { }
+    interface ElementClass extends Vue { }
+    interface IntrinsicElements {
+      [elem: string]: any
+    }
+  }
 }
